ff59f9785ff6eda11231fe65c0336b0b9525829d
chemdemo <yangdemo@gmail.com>
2014-01-24 23:45:25 +0800

    dev log

--- client/dev/js/main.js
+++ client/dev/js/main.js
@@ -1,16 +1,15 @@
 /**
  * main.js
- * Copyright (c) 2013-2014, dmyang (MIT License)
+ * Copyright (c) 2013-2014, dmyang<yangdemo@gmail.com> (MIT License)
  */
 
 'use strict';
 
 var DeskShell = module.exports = {};
 
-var fileMgr = require('./file_manager');
+var fileMgr = require('./FilePanel');
 var ui = require('./ui');
-var tmpl = require('./tmpl');
+var tmpl = require('./tmpl')['Tmpl'];
 var mock = require('./mock_data');
 
-// $('#path-tree').html(tmpl["Tmpl"]["pathTree"]({list: mock.pathList}));
-$('#files').html(tmpl["Tmpl"]["fileList"]({list: mock.fileList}));
+// $('#files').html(tmpl['fileList']({list: mock.fileList}));
